4730924ca05f86cd9336041c5244d6e2d76f105b
TiaApril <95060757+TiaApril@users.noreply.github.com>
2023-01-24 04:08:11 +0700

    Create number_guess.sql

---
+++ number_guess.sql
@@ -0,0 +1,73 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-2.pgdg20.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: players; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.players (
+    username character varying(22),
+    games_played numeric,
+    best_game numeric
+);
+
+
+ALTER TABLE public.players OWNER TO freecodecamp;
+
+--
+-- Data for Name: players; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.players VALUES ('user_1674505397701', 2, 449);
+INSERT INTO public.players VALUES ('user_1674505397702', 5, 122);
+INSERT INTO public.players VALUES ('user_1674505269711', 2, 428);
+INSERT INTO public.players VALUES ('user_1674505269712', 5, 284);
+INSERT INTO public.players VALUES ('Ani', 2, 12);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
